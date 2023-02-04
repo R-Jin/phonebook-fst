3340564adb7415d97b4256eed0434fc49be4f1c5
Ryan Jin <ryanjin007@gmail.com>
2023-02-04 18:09:00 +0100

    Changes to the port numbering

--- index.js
+++ index.js
@@ -95,8 +95,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person)
 
 })
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
