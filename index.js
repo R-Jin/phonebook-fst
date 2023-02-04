9fe842ff1934e6d75612054716e8a3d41a931e05
Ryan Jin <ryanjin007@gmail.com>
2023-02-04 17:57:20 +0100

    Complete exercise 3.9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { response } = require("express")
 const express = require("express")
+const cors = require("cors")
 var morgan = require("morgan")
 const app = express()
 
 morgan.token('post', function (req, res) { return JSON.stringify(req.body) })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :response-time :post'))
 
